5f115076daee3a8c93dba12c7e63303797113743
Cory Bergquist <cory.j.bergquist@gmail.com>
2018-06-28 17:08:23 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -16,7 +16,7 @@ class Pokemon
   end
 
   def self.find(id, db)
-    db.execute( "SELECT name FROM pokemon ORDER BY name" )
+    db.execute( "SELECT name FROM pokemon WHERE id = (?)," id )
   end
 
 
